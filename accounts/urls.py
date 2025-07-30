19179053b72489ff1589f7df9dcc1e26c6d14444
sanan <sanan@techgrowthkw.com>
2025-07-30 16:46:40 +0330

    feat: rename urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('auth/register/', RegisterUserView.as_view(), name='register-user-v1'),
     path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair-v1'),
     path('auth/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh-v1'),
-    path('accounts/change-password/', PasswordChangeView.as_view(), name='password-change'),
+    path('auth/change-password/', PasswordChangeView.as_view(), name='password-change'),
 ]
--- mahour_ai/urls.py
+++ mahour_ai/urls.py
@@ -6,9 +6,11 @@ from .swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('api/llm/', include('llm_caller.urls')),
-    path('api/', include('dashboard.urls')),
+    path('api/v1/', include([
+        path('accounts/', include('accounts.urls')),
+        path('llm/', include('llm_caller.urls')),
+        path('dashboard/', include('dashboard.urls')),
+    ])),
     path(
         'swagger/',
         login_required(schema_view.with_ui('swagger', cache_timeout=0)),
